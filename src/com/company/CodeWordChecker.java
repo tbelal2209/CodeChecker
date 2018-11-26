d69cc21fedc05cf246596dde2f78fcc802336f23
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-11-26 08:41:31 -0500

    Update CodeWordChecker.java

--- src/com/company/CodeWordChecker.java
+++ src/com/company/CodeWordChecker.java
@@ -1,24 +1,28 @@
 package com.company;
 
 public class CodeWordChecker implements StringChecker {
-private int max, min;
-private String str;
-public CodeWordChecker(String str, int max, int min){
-    this.max = max;
-    this.min = min;
-    this.str = str;
-}
-public int getMax(){
-    return max;
-}
-public int getMin(){
-    return min;
-}
+    private String pass;
+    private String $;
+    private int max, min;
+    private String str;
+    public CodeWordChecker(String str, int max, int min){
+        this.max = max;
+        this.min = min;
+        this.str = str;
+    }
+
+
+
+    public int getMax(){
+        return max;
+    }
+    public int getMin(){
+        return min;
+    }
     @Override
-    public boolean isValid(String str) {
-        if {
-
-
+    public boolean isValid(String illegalStr) {
+        if(this.str.indexOf(illegalStr) == 1) {
+        return true;
         }
         return false;
     }
